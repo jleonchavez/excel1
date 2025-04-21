18f6dbddc7a9201fc83c7090266ec9d1d7108299
Brandon-Leon <leon_gato_19@hotmail.com>
2025-04-21 11:33:39 -0600

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,42 @@
+$hours = 15
+$minutes = 0
+$PlusOrMinus = 1
+Add-Type -AssemblyName System.Windows.Forms
+if (-NOT ($hours -eq 0) -OR -NOT ($minutes -eq 0)) {
+  if (-NOT ($hours -eq 0)) {
+    $minutes = $minutes + ($hours * 60)
+  }
+  $sleepSecond = 5
+  $seconds = (($minutes * 60) / $sleepSecond)
+  $myshell = New-Object -com "Wscript.Shell"
+ 
+  for ($i = 0; $i -lt $seconds; $i++) {
+    $p = [System.Windows.Forms.Cursor]::Position
+    $x = $p.X + $PlusOrMinus
+    $y = $p.Y + $PlusOrMinus
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+    Start-Sleep -Seconds $sleepSecond
+    $total = ($i+1)*5
+    $letter = "$total Seconds"
+    if($total -ge 59) {
+     $letter = "Minutes"
+     $toRound = $total / 60
+     $minutes = [math]::Floor($toRound)
+     $scnds = $total - ($minutes * 60)
+     $letter = "$minutes $letter $scnds Seconds"
+     if($minutes -ge 60) {
+       $toRoundHours = $minutes /60
+       $hoursR = [math]::Floor($toRoundHours)
+       $newMinutes = $minutes - ($hoursR * 60)
+       $letter = "$hoursR Hours $newMinutes Minutes $scnds Seconds"
+     }
+    }
+    echo "$letter sleep"
+    $PlusOrMinus *= -1
+    $myshell.SendKeys("$letter")
+    $myshell.sendkeys("~")
+  } 
+} else {
+  echo "Add Minutes or Hours"
+  break;
+}
