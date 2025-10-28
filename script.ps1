06df48a774dc3bc019a0bb5deb76fbe0f9878448
jleonchavez <jonathan.leonchavez@metlifeexternos.com.mx>
2025-10-28 14:21:05 -0600

    Enhance sleep script with time checks and formatting
    Refactor sleep script to include time checks and formatting functions.

--- script.ps1
+++ script.ps1
@@ -1,42 +1,87 @@
 $hours = 15
 $minutes = 0
 $PlusOrMinus = 1
+$sleepSecond = 5
+
+function getDate($type){
+  if($type -eq 0){
+    return Get-Date -Format HH
+  }
+  if($type -eq 1){
+    return Get-Date -Format mm
+  }
+  if($type -eq 2){
+    return Get-Date -Format ss
+  }
+}
+
+function formatTime($time){
+  if($time -lt 10){
+    return "0$time"
+  }
+  return $time
+}
+
 Add-Type -AssemblyName System.Windows.Forms
 if (-NOT ($hours -eq 0) -OR -NOT ($minutes -eq 0)) {
   if (-NOT ($hours -eq 0)) {
     $minutes = $minutes + ($hours * 60)
   }
-  $sleepSecond = 5
+  
   $seconds = (($minutes * 60) / $sleepSecond)
   $myshell = New-Object -com "Wscript.Shell"
- 
   for ($i = 0; $i -lt $seconds; $i++) {
+    $currentHour = getDate(0)
+    $currentMinute = getDate(1)
+    $currentSecond = getDate(2)
+
+    if($currentHour -eq 14 -and $currentMinute -eq 30){
+      $sleepProgram = "true"
+      while ($sleepProgram -eq "true"){
+        $currentHour = getDate(0)
+        $currentMinute = getDate(1)
+        if($currentHour -eq 15 -and $currentMinute -ge 50){
+          $sleepProgram = "false"
+        }
+        Start-Sleep -Seconds 1
+      }
+    }
+
+    if($currentHour -eq 18 -and  $currentMinute -ge 10){
+      Write-Output "Bye Bye..."
+      break;
+    }
+
     $p = [System.Windows.Forms.Cursor]::Position
     $x = $p.X + $PlusOrMinus
     $y = $p.Y + $PlusOrMinus
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
     Start-Sleep -Seconds $sleepSecond
-    $total = ($i+1)*5
+    $total = ($i+1)*$sleepSecond
     $letter = "$total Seconds"
-    if($total -ge 59) {
-     $letter = "Minutes"
+    $letter = formatTime($total) "Seconds"
+    if($total -ge 60) {
+     $letter = ""
      $toRound = $total / 60
      $minutes = [math]::Floor($toRound)
      $scnds = $total - ($minutes * 60)
-     $letter = "$minutes $letter $scnds Seconds"
+     $scnds = formatTime($scnds)
+     $minutes = formatTime($minutes)
+     $letter = "$minutes Minutes $scnds Seconds"
      if($minutes -ge 60) {
        $toRoundHours = $minutes /60
        $hoursR = [math]::Floor($toRoundHours)
        $newMinutes = $minutes - ($hoursR * 60)
+       $newMinutes = formatTime($newMinutes)
        $letter = "$hoursR Hours $newMinutes Minutes $scnds Seconds"
      }
     }
-    echo "$letter sleep"
+    Write-Output "$letter sleep - $currentHour : $currentMinute : $currentSecond"
     $PlusOrMinus *= -1
-    $myshell.SendKeys("$letter")
+    $myshell.SendKeys("$letter sleep")
     $myshell.sendkeys("~")
   } 
 } else {
-  echo "Add Minutes or Hours"
+  Write-Output "Add Minutes or Hours"
   break;
 }
